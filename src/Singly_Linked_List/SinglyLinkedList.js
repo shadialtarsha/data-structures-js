5e5a7a1a64b06af9b8f1003ff8fb7aafa85964c0
shadialtarsha <shadi.altarsha.94@gmail.com>
2019-07-11 18:10:31 +0300

    Add SinglyLinkedList

---
+++ src/Singly_Linked_List/SinglyLinkedList.js
@@ -0,0 +1,142 @@
+const Node = require('./Node');
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return;
+    }
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) {
+      return;
+    }
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    const node = this.get(index);
+    if (node) {
+      node.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === 0) {
+      return !!this.unshift(val);
+    }
+    if (index === this.length) {
+      return !!this.push(val);
+    }
+    const newNode = new Node(val);
+    const preNode = this.get(index - 1);
+    newNode.next = preNode.next;
+    preNode.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      return;
+    }
+    if (index === 0) {
+      return this.shift();
+    }
+    if (index === this.length - 1) {
+      return this.pop();
+    }
+    const preNode = this.get(index - 1);
+    const removed = preNode.next;
+    preNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let nextNode;
+    let prevNode = null;
+    while (node) {
+      nextNode = node.next;
+      node.next = prevNode;
+      prevNode = node;
+      node = nextNode;
+    }
+    return this;
+  }
+}
+
+module.exports = SinglyLinkedList;
---
+++ src/Singly_Linked_List/index.js
@@ -0,0 +1,8 @@
+const SinglyLinkedList = require('./SinglyLinkedList');
+
+const list = new SinglyLinkedList();
+list.push('HELLO');
+list.push('GOODBYE');
+list.push('!');
+list.push('<3');
+list.push(':)');
